abf0cd1d4780b233e4a0b71176a02a3af1522371
Nicolas Gramlich <ngramlich@zynga.com>
2011-08-15 16:40:25 -0400

    Starting 'GLES2' branch.

---
+++ src/org/anddev/andengine/extension/texturepacker/opengl/texture/util/texturepacker/TexturePackTextureRegionLibrary.java
@@ -0,0 +1,43 @@
+package org.anddev.andengine.extension.texturepacker.opengl.texture.util.texturepacker;
+
+import org.anddev.andengine.util.Library;
+
+/**
+ * (c) Zynga 2011
+ *
+ * @author Nicolas Gramlich <ngramlich@zynga.com>
+ * @since 16:34:23 - 15.08.2011
+ */
+public class TexturePackTextureRegionLibrary extends Library<TexturePackerTextureRegion> {
+	// ===========================================================
+	// Constants
+	// ===========================================================
+
+	// ===========================================================
+	// Fields
+	// ===========================================================
+
+	// ===========================================================
+	// Constructors
+	// ===========================================================
+
+	public TexturePackTextureRegionLibrary(final int pInitialCapacity) {
+		super(pInitialCapacity);
+	}
+
+	// ===========================================================
+	// Getter & Setter
+	// ===========================================================
+
+	// ===========================================================
+	// Methods for/from SuperClass/Interfaces
+	// ===========================================================
+
+	// ===========================================================
+	// Methods
+	// ===========================================================
+
+	// ===========================================================
+	// Inner and Anonymous Classes
+	// ===========================================================
+}
